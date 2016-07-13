6df6ace8791f21a5a5b94b7abdf78c992205651b
gleisoncs <gleisoncs@gmail.com>
2016-07-13 13:24:54 -0300

    initial version

---
+++ src/main/java/com/gtech/onepage/HomeController.java
@@ -0,0 +1,36 @@
+package com.gtech.onepage;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.gtech.onepage.model.GenericResult;
+import com.gtech.onepage.service.UserService;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	UserService crmService;
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String search(Model model) {
+		Long timeLimit = System.currentTimeMillis();
+		List<GenericResult> result1List = Collections.emptyList();
+
+		try {
+			//result1List = crmService.getAccount("");
+		} catch (Exception e) {
+		}
+		model.addAttribute("result1List", result1List);
+
+		System.out.println(String.format("Executou em %s milisegundos.", System.currentTimeMillis() - timeLimit));
+
+		return "home";
+	}
+}
